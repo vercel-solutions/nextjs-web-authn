e1d47ee1725d26fa84848be62a59f53e33d41926
Lee Robinson <lrobinson2011@gmail.com>
2021-03-21 19:37:48 -0500

    Add OG information.

---
+++ pages/_document.js
@@ -0,0 +1,41 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    const meta = {
+      title: 'Web Authn – Next.js and Magic Example',
+      description: 'Login with Yubikey or TouchID on your Chrome browser.',
+      image:
+        'https://og-image.vercel.app/**WebAuthn%20Login**%20with%20Magic.png?theme=dark&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-white-logo.svg&images=https%3A%2F%2Fsvgshare.com%2Fi%2FVHH.svg',
+    };
+
+    return (
+      <Html>
+        <Head>
+          <meta name="robots" content="follow, index" />
+          <meta name="description" content={meta.description} />
+          <meta property="og:site_name" content={meta.title} />
+          <meta property="og:description" content={meta.description} />
+          <meta property="og:title" content={meta.title} />
+          <meta property="og:image" content={meta.image} />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content="@vercel" />
+          <meta name="twitter:title" content={meta.title} />
+          <meta name="twitter:description" content={meta.description} />
+          <meta name="twitter:image" content={meta.image} />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
